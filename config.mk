3c6c783fe6e661810b5dc41fb357221c284abd88
Anay Wadhera <anay1018@gmail.com>
2022-12-09 12:51:26 +0900

    fortune: Adapt clock font to qpr1
    Change-Id: Ie369cdc9e1f491e2326036e90270c625b04452b6
    Reviewed-on: https://review.statixos.com/c/android_vendor_statix/+/9118
    Reviewed-by: Anay Wadhera <anay1018@gmail.com>
    Tested-by: Anay Wadhera <anay1018@gmail.com>
    Reviewed-by: Sourajit Karmakar <sourajitk@statixos.com>

--- config.mk
+++ config.mk
@@ -24,6 +24,7 @@ endif
 PRODUCT_COPY_FILES += \
     vendor/fortune-extras/fonts/GoogleSans-Regular.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-Regular.ttf \
     vendor/fortune-extras/fonts/GoogleSans-Italic.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-Italic.ttf \
+    vendor/fortune-extras/fonts/GoogleSansClock-Regular.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSansClock-Regular.ttf \
     vendor/fortune-extras/fonts/fonts_customization.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/fonts_customization.xml
 
 # Gapps

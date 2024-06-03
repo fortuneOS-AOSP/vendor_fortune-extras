9cf5ebba3bb02ae7533506d10ed49df4f57291d1
Mikazuuu07 <mikazuuu07@komodo-os.my.id>
2024-06-03 03:50:09 +0000

    fortune: Import our default wallpaper
    Change-Id: Iceea8ae793006e69eee582fb94dea1c4d68d0343

--- config.mk
+++ config.mk
@@ -12,3 +12,7 @@ PRODUCT_PACKAGES += \
     charger_res_images \
     product_charger_res_images \
     product_charger_res_images_vendor
+
+# Overlay
+PRODUCT_PACKAGE_OVERLAYS += vendor/fortune-extras/overlay/common
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/fortune-extras/overlay/common

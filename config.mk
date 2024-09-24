0fa7d27bd6ed2864c76dc003976b9f7a3600c694
Kizziama <kizziama@proton.me>
2024-09-24 09:09:46 +0000

    fortune: Inherit gapps config
    Change-Id: Iac542126249e60114f562ddfd33035fdd3a11016

--- config.mk
+++ config.mk
@@ -4,6 +4,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Gapps
+$(call inherit-product, vendor/gms/products/gms.mk)
+
 # Overlays
 PRODUCT_PACKAGE_OVERLAYS += \
     vendor/fortune-extras/overlay/common
